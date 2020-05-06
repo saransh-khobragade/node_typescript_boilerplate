2244434d9c5418638ca18d1a3a2d71e276659750
Saransh <saransh98@gmail.com>
2020-05-06 17:44:54 +0530

    master code working

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import compression from "compression";  // compresses requests
+import bodyParser from "body-parser";
+import lusca from "lusca";
+import flash from "express-flash";
+import path from "path";
+
+// Controllers (route handlers)
+import * as apiController from "./controllers/api";
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(flash());
+app.use(lusca.xframe("SAMEORIGIN"));
+app.use(lusca.xssProtection(true));
+app.use((req, res, next) => {
+    res.locals.user = req.user;
+    next();
+});
+
+app.use(
+    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+);
+
+/**
+ * API examples routes.
+ */
+app.get("/api", apiController.getApi);
+
+
+export default app;
