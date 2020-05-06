cf328b3a7a78d13f6dbf65f14595fd2d4a2d8941
Saransh <saransh98@gmail.com>
2020-05-06 22:19:42 +0530

    everything working

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,7 +1,7 @@
 "use strict";
 
 import { Response, Request, NextFunction } from "express";
-import { Sample_type } from "../types/sample_type"
+import { SampleType } from "../types/sample_type";
 
 
 /**
@@ -10,7 +10,7 @@ import { Sample_type } from "../types/sample_type"
  */
 export const getApi = (req: Request, res: Response) => {
 
-    const static_type_obj:Sample_type = { "firstName":"ss","lastName":"ss" }
+    const staticTypeObj: SampleType = { "firstName":"ss","lastName":"ss" };
 
-    res.status(200).send({"message":static_type_obj})
+    res.status(200).send({"message":staticTypeObj});
 };
--- src/types/sample_type.ts
+++ src/types/sample_type.ts
@@ -1,4 +1,4 @@
-export interface Sample_type {
+export interface SampleType {
     firstName: string;
     lastName: string;
 }
